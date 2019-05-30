22ae91edf4422d836675a68250d8b4dc90a746ed
vigism <rushilvig@gmail.com>
2019-05-30 14:31:31 -0700

    did some queries

--- activerecord/movie_buff/01_queries.rb
+++ activerecord/movie_buff/01_queries.rb
@@ -6,7 +6,7 @@ def it_was_ok
   # We can use ranges (a..b) inside a where method.
   #
   # Find the id, title, and score of all movies with scores between 2 and 3
-
+  Movie.select(:id,:score,:title).where(score: 2.0..3.0)
 end
 
 def harrison_ford
@@ -20,7 +20,7 @@ def harrison_ford
   #
   # Find the id and title of all movies in which Harrison Ford
   # appeared but not as a lead actor
-
+  Movie.select(:id,:title).joins(:actors).where(actors: {name:'Harrison Ford'}).where.not(castings: {ord: 1}).limit(10)
 end
 
 def biggest_cast
@@ -37,6 +37,7 @@ def biggest_cast
   #
   # Find the id and title of the 3 movies with the
   # largest casts (i.e most actors)
+  Movie.select(:id, :title).joins(:actors).group('movies.id').order('COUNT(actors.id) DESC').limit(3)
 
 end
 
